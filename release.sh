43db1f33d4b4476a95796f4cfbd0a919431691ee
xpy123993 <xpy123993@gmail.com>
2023-02-19 11:05:11 -0800

    Update corenet version.

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ VERSION_NUMBER=${1:-debug}
 
 [ ! -d $OUTPUT_FOLDER ] && mkdir $OUTPUT_FOLDER
 
-for ARCH in arm arm64 amd64; do
+for ARCH in arm64 amd64; do
 CGO_ENABLED=0 GOOS=linux GOARCH=$ARCH go build -ldflags "-s -w" -o $OUTPUT_FOLDER/clover3-${VERSION_NUMBER}.${ARCH} $SRC_FOLDER
 upx $OUTPUT_FOLDER/clover3-${VERSION_NUMBER}.${ARCH}
 done
